b77a8801e55ba8daf83b62f7a2743cc32f72206d
Olivier D <olivier.dauloudet@hotmail.com>
2022-04-12 01:33:39 +0200

    add state info obj

---
+++ src/ssurgo_provider/object/state_info.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+
+class StateInfo:
+    def __init__(self, state_code, points, status):
+        self.status = status
+        self.state_code = state_code
+        self.points = points
+        self.state_folder_pth = None
+        self.soil_data = None
+
+    def set_soil(self, soil_data):
+        self.soil_data = soil_data
+        self.status = StateInfoStatus.SUCCEED
+
+
+class StateInfoStatus(Enum):
+    NOT_IN_USA = "FAILED"
+    NO_GDB_FILE_FOUND = "FAILED"
+    SUCCEED = "SUCCEED"
+    IN_PROGRESS = "PENDING"
